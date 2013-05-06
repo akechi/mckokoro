202de868c464adff96bee465cd8bbc5163e06acd
ujihisa <ujihisa@gmail.com>
2013-05-05 17:24:47 -0700

    1.5.2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject mckokoro "1.0.0-SNAPSHOT"
   :description "a minecraft server in JRuby w/o purugin"
-  :dependencies [[org.bukkit/bukkit "1.5.1-R0.3-SNAPSHOT"]
+  :dependencies [[org.bukkit/bukkit "1.5.2-R0.1-SNAPSHOT"]
                  [org.jruby/jruby-complete "1.7.3"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.5.1-R0.3-SNAPSHOT"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.5.2-R0.1-SNAPSHOT"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
   :javac-options ["-d" "classes/" "-Xlint:deprecation"]
