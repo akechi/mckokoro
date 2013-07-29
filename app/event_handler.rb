00020b1b14664aec5a4af8159cbb3fa0f5d8daf3
supermomonga <hasegawa@supermomonga.com>
2013-07-30 06:08:54 +0900

    explode clicked block

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -206,7 +206,7 @@ module EventHandler
         when Material::TNT
           # killerqueen...!!
           evt.player.send_message "KILLERQUEEN...!!"
-          explode(evt.player.getLocation, 1, false)
+          explode(evt.clicked_block.getLocation, 1, false)
         end
       end
     else
