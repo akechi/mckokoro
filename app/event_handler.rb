c7280a88bb7bf8ced766a40685da588202f80157
supermomonga <hi@supermomonga.com>
2013-08-02 07:16:33 +0900

    hayasugiru...

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -377,7 +377,7 @@ module EventHandler
       if evt.player.location.clone.add(0, -1, 0).block.type == Material::SAND
         evt.cancelled = true
       else
-        evt.player.walk_speed = 0.5
+        evt.player.walk_speed = 0.4
       end
     else
       evt.player.walk_speed = 0.2
