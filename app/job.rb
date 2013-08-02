35f8b287f87bf21d2cb49ac1f4b97de3db25da67
ujihisa <ujihisa@gmail.com>
2013-08-01 22:18:07 -0700

    %s...?

--- app/job.rb
+++ app/job.rb
@@ -14,7 +14,7 @@ module Job
   end
 
   def become(player, new_job)
-    unless %s[killerqueen].include? new_job
+    unless [:killerqueen].include? new_job
       warn "job #{new_job.inspect} isn't available"
       return
     end
