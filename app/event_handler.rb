001af214a53ea4410deab5563cd81cfb17832941
ujihisa <ujihisa@gmail.com>
2013-08-03 18:45:43 -0700

    checks total number of the items

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -217,7 +217,10 @@ module EventHandler
     player.send_message 'Success!!!'
     sizev = v2 - v1
     sizew = w2 - w1
-    if sizev * sizew > 100
+    itemstacks = player.inventory.contents.to_a.compact.select {|is|
+      is.type == block1.type
+    }
+    if sizev * sizew > itemstacks.map(&:amount).inject(:+)
       player.send_message "Failed! the size is too big #{sizev}*#{sizew}"
       false
     else
