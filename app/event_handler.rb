73eaa2c0fbeddf41a1b9b4a4b9f415b2bd71bddd
ujihisa <ujihisa@gmail.com>
2013-08-04 13:32:51 -0700

    sand

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -430,7 +430,8 @@ module EventHandler
         evt.entity.shooter.item_in_hand.type == Material::GOLD_HOE
       if cond
         soft_blocks =
-          [Material::GRASS, Material::DIRT, Material::GRAVEL, Material::STONE]
+          [Material::GRASS, Material::DIRT, Material::GRAVEL, Material::STONE,
+           Material::SAND]
         location_around(evt.entity.location, 1).each do |loc|
           b = loc.block
           cond =
