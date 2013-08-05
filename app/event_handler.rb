18644c9618f6c222f4156954528956dd66ce6a28
ujihisa <ujihisa@gmail.com>
2013-08-04 18:15:36 -0700

    java is terrible

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1,3 +1,4 @@
+import 'java.util.HashSet'
 import 'org.bukkit.Bukkit'
 import 'org.bukkit.Material'
 import 'org.bukkit.Effect'
@@ -475,7 +476,8 @@ module EventHandler
     return unless player.item_in_hand.type == Material::GOLD_SWORD
     case action
     when Action::LEFT_CLICK_BLOCK, Action::LEFT_CLICK_AIR
-      block1, block2 = player.get_last_two_target_blocks([Material::AIR].to_set, 30)
+      transparent_set = HashSet.new.tap {|s| s.add Material::AIR }
+      block1, block2 = player.get_last_two_target_blocks(transparent_set, 30)
       player.send_message [block1, block2].to_s
     end
   end
