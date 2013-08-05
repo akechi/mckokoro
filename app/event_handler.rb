0e5dfb7a6b580fd27908fb16dee13aaee6d87142
ujihisa <ujihisa@gmail.com>
2013-08-04 17:55:04 -0700

    hmm

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -659,7 +659,7 @@ module EventHandler
             evt.damage *= 2
           end
         when Skeleton
-          evt.damage *= 2
+          evt.damage *= 5
         end
       end
     when Snowball
