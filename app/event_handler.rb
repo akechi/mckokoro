4cc098ff6f0e40703c77b3171db90c211866bb1c
ujihisa <ujihisa@gmail.com>
2013-08-07 00:28:59 -0700

    debug

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -480,6 +480,9 @@ module EventHandler
       # this is for Job bulldozer ^^^^^
     when Arrow
       loc = evt.entity.location
+      if evt.entity.shooter && Player === evt.entity.shooter
+        ev.entity.shooter.send_message "destination: #{loc.block}"
+      end
       if loc.block.type == Material::SKULL
         strike_lightning(loc)
       end
