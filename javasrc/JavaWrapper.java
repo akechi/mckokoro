e8bf1997710615f47f74cac1f389f3eeb34f5b20
ujihisa <ujihisa@gmail.com>
2013-08-08 17:04:42 -0700

    rest of launch stuff

--- javasrc/JavaWrapper.java
+++ javasrc/JavaWrapper.java
@@ -10,7 +10,39 @@ public class JavaWrapper {
     return e.launchProjectile(Egg.class);
   }
 
+  static EnderPearl launchEnderPearl(LivingEntity e) {
+    return e.launchProjectile(EnderPearl.class);
+  }
+
+  static Fireball launchFireball(LivingEntity e) {
+    return e.launchProjectile(Fireball.class);
+  }
+
+  static Fish launchFish(LivingEntity e) {
+    return e.launchProjectile(Fish.class);
+  }
+
+  static LargeFireball launchLargeFireball(LivingEntity e) {
+    return e.launchProjectile(LargeFireball.class);
+  }
+
+  static SmallFireball launchSmallFireball(LivingEntity e) {
+    return e.launchProjectile(SmallFireball.class);
+  }
+
   static Snowball launchSnowball(LivingEntity e) {
     return e.launchProjectile(Snowball.class);
+  }
+
+  static ThrownExpBottle launchThrownExpBottle(LivingEntity e) {
+    return e.launchProjectile(ThrownExpBottle.class);
+  }
+
+  static ThrownPotion launchThrownPotion(LivingEntity e) {
+    return e.launchProjectile(ThrownPotion.class);
+  }
+
+  static WitherSkull launchWitherSkull(LivingEntity e) {
+    return e.launchProjectile(WitherSkull.class);
   }
 }
