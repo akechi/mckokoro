c505324524470c5ab694cc7790e2ac8eb0351fbb
supermomonga <hi@supermomonga.com>
2013-08-12 19:29:44 +0900

    reset all velocity

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -876,7 +876,11 @@ module EventHandler
       if v_orbs[n] && v_orbs[n].valid?
         orb = v_orbs[n]
         orb.teleport orb_loc
-        orb.velocity = orb.velocity.set_y jfloat(0.0)
+        orb.velocity = orb.velocity.tap { |v| 
+          v.set_x jfloat(0.0)
+          v.set_y jfloat(0.0)
+          v.set_z jfloat(0.0)
+        }
       else
         orb = spawn(orb_loc, EntityType::SNOWBALL)
         # TODO find the best entity to use visual orbs
