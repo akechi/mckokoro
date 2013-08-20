061aeadc390560666c90c2f9ac907f96585e4f1f
ujihisa <ujihisa@gmail.com>
2013-08-20 05:14:41 -0700

    less

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -949,7 +949,7 @@ module EventHandler
       when [Material::LOG, Action::RIGHT_CLICK_BLOCK, Material::SHEARS]
         consume_item_durability(evt.player, 1)
         if rand(5) == 0
-          if true || rand(20) == 0
+          if rand(2) == 0 # 1/(5*2) possibility
             evt.clicked_block.type = Material::WOOD
             evt.clicked_block.data = evt.clicked_block.state.data.species.data
           end
