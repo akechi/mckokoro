76034b46521b4a626fda6fcbe13e110bc76b923e
ujihisa <ujihisa@gmail.com>
2013-08-28 00:15:26 -0700

    hmm..?

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -969,8 +969,8 @@ module EventHandler
     return unless [Action::LEFT_CLICK_BLOCK, Action::LEFT_CLICK_AIR].include?(action)
     vehicle.velocity = vehicle.velocity.tap {|v|
       y = v.get_y
-      v.multiply(jfloat(2.0))
-      v.set_y(jfloat(y + 0.1))
+      v.multiply(jfloat(3.0))
+      v.set_y(jfloat(y + 0.5))
     }
   end
   private :horse_sword_swing
