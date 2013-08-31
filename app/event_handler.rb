caebccc50f7a21d24cc8d2e40e06a6db874df0c4
ujihisa <ujihisa@gmail.com>
2013-08-31 00:38:53 -0700

    more more

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1012,7 +1012,7 @@ module EventHandler
           play_sound(player.location, Sound::PIG_IDLE, 0.8, 0.0)
           play_sound(player.location, Sound::PIG_IDLE, 0.8, 2.0)
           player.velocity = player.velocity.tap {|v|
-            v.multiply(3.0)
+            v.multiply(10.0)
             v.set_y(v.get_y + 1.5)
           }
         end
