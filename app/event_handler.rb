e81d6a5a2641d56369c4096c0bab66a71bf60324
ujihisa <ujihisa@gmail.com>
2013-09-05 23:36:30 -0700

    sound adjustment

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1794,7 +1794,7 @@ module EventHandler
         @crouching_counter[name] -= 1
       end
       if @crouching_counter[name] == 5
-        play_sound(player.location, Sound::EXPLODE, 1.0, 1.5)
+        play_sound(add_loc(player.location, 0, 5, 0), Sound::GHAST_CHARGE, 1.0, 1.5)
         # evt.player.send_message "superjump!"
         player.fall_distance = 0.0
         player.velocity = player.velocity.tap {|v| v.set_y jfloat(1.4) }
