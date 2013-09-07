45c710b19dfe1f7eaac649681f07d5a041c0708b
ujihisa <ujihisa@gmail.com>
2013-09-06 23:13:46 -0700

    kiken

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1701,7 +1701,7 @@ module EventHandler
       evt.cancelled = true
       later 0 do
         entity.velocity = entity.velocity.tap {|v|
-          v.set_y([Math.log(falld), 5.0].min)
+          v.set_y([Math.log(falld) * 0.5, 5.0].min)
         }
       end
     end
