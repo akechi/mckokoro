ba0edbf1298afbb9764db60415a75ea8202a1fd1
ujihisa <ujihisa@gmail.com>
2013-09-06 22:28:51 -0700

    optifine

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -297,7 +297,9 @@ module EventHandler
       post_lingr_to('computer_science', "#{evt.player.name}: #{message}")
     else
       post_lingr("#{evt.player.name}: #{evt.message}")
-      evt.player.world.weather_duration = 0 if /optifine/ =~ evt.message
+      if /optifine/ =~ evt.message
+        Bukkit.dispatch_command(Bukkit.get_console_sender, "toggledownfall")
+      end
     end
   end
 
