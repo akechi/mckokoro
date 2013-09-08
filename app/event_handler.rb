fa46311ae666aa1b92189ef5f190412951f7805a
ujihisa <ujihisa@gmail.com>
2013-09-07 23:36:51 -0700

    teleport

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1505,8 +1505,9 @@ module EventHandler
       }
       later 0 do
         entities.each do |e|
+          e.teleport(add_loc(e.location, 0, 1, 0))
           e.velocity = e.velocity.tap {|v|
-            v.set_y 0.8
+            v.set_y 0.7
           }
         end
       end
