1c9fa5cf445b46df91f10500c85eee60367f2849
ujihisa <ujihisa@gmail.com>
2013-09-07 23:23:09 -0700

    adjustment

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1506,7 +1506,7 @@ module EventHandler
       later 0 do
         entities.each do |e|
           e.velocity = e.velocity.tap {|v|
-            v.set_y 0.8
+            v.set_y 0.7
           }
         end
       end
