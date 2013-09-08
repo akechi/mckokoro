fc111e70b6b12e6e6d987b8f8b0052581fd7ed59
ujihisa <ujihisa@gmail.com>
2013-09-08 00:03:02 -0700

    no debug msg

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1683,7 +1683,6 @@ module EventHandler
             evt.cancelled = true
           else
             evt.damager.damage(evt.damage, defender)
-            defender.send_message "counter attack! #{evt.damage} damage to enemy(#{evt.damager.health})"
             evt.cancelled = true
           end
         end
