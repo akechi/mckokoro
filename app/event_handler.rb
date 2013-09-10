f8096f1a7c0f19650fdf7121961619d2d19a6725
ujihisa <ujihisa@gmail.com>
2013-09-09 19:07:34 -0700

    flip

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1163,7 +1163,7 @@ module EventHandler
         evt.player.send_message "#{evt.clicked_block.state.data.attached_face}"
         let(evt.clicked_block.state) do |state|
           d = state.data
-          d.facing_direction = BlockFace::WEST
+          d.facing_direction = d.facing_direction.opposite_face
           state.data = d
           state.update()
         end
