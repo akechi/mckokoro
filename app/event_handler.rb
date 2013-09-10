eb39ec11cccb2ef4c7cecde9f44acfba70875fdf
ujihisa <ujihisa@gmail.com>
2013-09-09 21:54:55 -0700

    clockwise

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -226,24 +226,13 @@ module Util
 
   def facing_next(facing)
     all_facings = [
-      BlockFace::EAST_SOUTH_EAST,
-      BlockFace::EAST,
-      BlockFace::EAST_NORTH_EAST,
-      BlockFace::NORTH_EAST,
-      BlockFace::NORTH_NORTH_EAST,
-      BlockFace::NORTH,
-      BlockFace::NORTH_NORTH_WEST,
-      BlockFace::NORTH_WEST,
-      BlockFace::WEST_NORTH_WEST,
-      BlockFace::WEST,
-      BlockFace::WEST_SOUTH_WEST,
-      BlockFace::SOUTH_WEST,
-      BlockFace::SOUTH_SOUTH_WEST,
-      BlockFace::SOUTH,
-      BlockFace::SOUTH_SOUTH_EAST,
-      BlockFace::SOUTH_EAST,
-    ]
-    all_facings[(all_facings.find_index(facing) + 1) % all_facings.size]
+      BlockFace::EAST_SOUTH_EAST, BlockFace::EAST, BlockFace::EAST_NORTH_EAST,
+      BlockFace::NORTH_EAST, BlockFace::NORTH_NORTH_EAST, BlockFace::NORTH,
+      BlockFace::NORTH_NORTH_WEST, BlockFace::NORTH_WEST,
+      BlockFace::WEST_NORTH_WEST, BlockFace::WEST, BlockFace::WEST_SOUTH_WEST,
+      BlockFace::SOUTH_WEST, BlockFace::SOUTH_SOUTH_WEST, BlockFace::SOUTH,
+      BlockFace::SOUTH_SOUTH_EAST, BlockFace::SOUTH_EAST]
+    all_facings[(all_facings.find_index(facing) - 1) % all_facings.size]
   end
 
   def silence_warnings
