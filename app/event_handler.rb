1c07ce1b40bf962498bd69698ebcd654a7c1cf98
ujihisa <ujihisa@gmail.com>
2013-09-10 00:47:01 -0700

    ?

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -112,8 +112,8 @@ module Util
 
   def face2pitchyaw(face)
     x, z = face.mod_x, face.mod_z
-    phi = Math.atan2(x, z) * 180 / Math::PI
-    [0.0, phi + 90]
+    phi = Math.atan2(-x, z) * 180 / Math::PI
+    [0.0, phi]
   end
 
   def stochastically(percentage)
