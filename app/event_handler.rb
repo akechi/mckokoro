e08ffe209683e2b064e6963c45813d24daa7e182
ujihisa <ujihisa@gmail.com>
2013-09-10 19:14:21 -0700

    hmm

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -663,16 +663,18 @@ module EventHandler
       end
     when Material::PUMPKIN, Material::JACK_O_LANTERN
       base_loc = evt.block_placed.location
-      mod_x, mod_z =
-        let(evt.block_placed.state.data.facing) {|f| [f.mod_x, f.mod_z] }
-      cond =
-        add_loc(base_loc, 0, -1, 0).block.type == Material::DIAMOND_BLOCK &&
-        add_loc(base_loc, mod_x, -1, mod_z).block.type == Material::DIAMOND_BLOCK &&
-        add_loc(base_loc, -mod_x, -1, -mod_z).block.type == Material::DIAMOND_BLOCK &&
-        add_loc(base_loc, 0, -2, 0).block.type == Material::DIAMOND_BLOCK
-      if cond
-        evt.cancelled = true
-        player.send_message "Success! Diamond golem! #{mod_x} #{mod_z}"
+      later 0 do
+        mod_x, mod_z =
+          let(evt.block_placed.state.data.facing) {|f| [f.mod_x, f.mod_z] }
+        cond =
+          add_loc(base_loc, 0, -1, 0).block.type == Material::DIAMOND_BLOCK &&
+          add_loc(base_loc, mod_x, -1, mod_z).block.type == Material::DIAMOND_BLOCK &&
+          add_loc(base_loc, -mod_x, -1, -mod_z).block.type == Material::DIAMOND_BLOCK &&
+          add_loc(base_loc, 0, -2, 0).block.type == Material::DIAMOND_BLOCK
+        if cond
+          #evt.cancelled = true
+          player.send_message "Success! Diamond golem! #{mod_x} #{mod_z}"
+        end
       end
     end
   end
