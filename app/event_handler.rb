ead202fa68673b8a9d3fd85c721c909b1a75b7c5
ujihisa <ujihisa@gmail.com>
2013-09-14 21:59:06 -0700

    test

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1770,14 +1770,18 @@ module EventHandler
       when Player
         sword_blocking_counter_attack(evt, defender, player)
         if @ctf_players.member?(player) && @ctf_players.member?(defender)
-          smoke_effect(defender.location)
-          evt.damage = 0
-          later sec(0.5) do
-            strike_lightning(defender.location)
-            defender.teleport(defender.location.tap {|l|
-              l.set_x(-535)
-              l.set_z(153)
-            })
+          if defender.passenger && Squid === defender.passenger
+            defender.eject
+          else
+            smoke_effect(defender.location)
+            evt.damage = 0
+            later sec(1.0) do
+              strike_lightning(defender.location)
+              defender.teleport(defender.location.tap {|l|
+                l.set_x(-535)
+                l.set_z(153)
+              })
+            end
           end
           return
         end
