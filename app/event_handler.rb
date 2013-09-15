d22cc2aee71d4376f2aa612e21e9f62a5cda2e28
ujihisa <ujihisa@gmail.com>
2013-09-15 10:23:58 -0700

    ES

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1356,7 +1356,7 @@ module EventHandler
           if safety_loc
             pitch, yaw = face2pitchyaw(face)
             safety_loc.set_yaw(yaw + 180)
-            strike_lightning(safety_loc)
+            play_effect(safety_loc, Effect::ENDER_SIGNAL)
             player.teleport safety_loc
             broadlingr "#{player.name} teleported to #{name}"
           else
