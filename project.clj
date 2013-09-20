6386f9fc15b7084bb81f18e183d18047af3abdf0
ujihisa <ujihisa@gmail.com>
2013-09-19 21:06:50 -0700

    1.6.2->1.6.4

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject mckokoro "1.0.0-SNAPSHOT"
   :description "a minecraft server in JRuby w/o purugin"
-  :dependencies [[org.bukkit/bukkit "1.6.2-R1.1-SNAPSHOT"]
+  :dependencies [[org.bukkit/bukkit "1.6.4-R0.1-SNAPSHOT"]
                  [org.jruby/jruby-complete "1.7.3"]]
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dev-dependencies [[org.bukkit/bukkit "1.6.2-R1.1-SNAPSHOT"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
   :javac-options ["-d" "classes/" "-Xlint:deprecation"]
