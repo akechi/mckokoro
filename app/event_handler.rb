fbac9d54f393070e48ed9c1461e1b04bc03eb90d
ujihisa <ujihisa@gmail.com>
2013-09-21 10:05:13 -0700

    oops

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1805,7 +1805,7 @@ module EventHandler
 
   def player_damages_zombie(player, zombie, evt)
     # To avoid minecraft and bukkit's suspicious bug.
-    return if evt.damage == 1.0
+    return if evt.damage >= 1.2 && 0.7 >= evt.damage
     evt.damage = jfloat(1.0)
     cur_health = zombie.health
     later 0 do
