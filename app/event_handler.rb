19d22fba0e8da8550f2c1630b27bc6458a1bf95a
ujihisa <ujihisa@gmail.com>
2013-09-22 22:03:23 -0700

    ksk

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1111,11 +1111,11 @@ module EventHandler
     later 0 do
       entities_on_the_door.each do |p|
         p.velocity = p.velocity.tap {|v|
-          v.add Vector.new(facing.mod_x * 3.0, 1.0, facing.mod_z * 3.0)
+          v.add Vector.new(facing.mod_x * 3.0, 0.8, facing.mod_z * 3.0)
         }
         later sec(0.5) do
           p.velocity = p.velocity.tap {|v|
-            v.add Vector.new(facing.mod_x * 3.0, 1.0, facing.mod_z * 3.0)
+            v.add Vector.new(facing.mod_x * 2.0, 0.8, facing.mod_z * 2.0)
           }
         end
       end
