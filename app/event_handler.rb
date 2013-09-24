714d51b38d73e3edf09c94f8ef80816c8578c959
ujihisa <ujihisa@gmail.com>
2013-09-23 22:03:12 -0700

    ok

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -333,7 +333,10 @@ module EventHandler
       post_lingr_to('computer_science', "#{last_pname}: #{last_message}")
       post_lingr_to('computer_science', "#{evt.player.name}: #{message}")
     else
-      evt.message = evt.message.gsub(/[bv]enr[iy]/, '便利').gsub(/[fh]u[bv]en/, '不便')
+      evt.message = evt.message.
+        gsub(/[fh]u[bv]enr[iy]/, '不便利').
+        gsub(/[bv]enr[iy]/, '便利').
+        gsub(/[fh]u[bv]en/, '不便')
       post_lingr("#{evt.player.name}: #{evt.message}")
       if /optifine/ =~ evt.message
         Bukkit.dispatch_command(Bukkit.get_console_sender, "toggledownfall")
