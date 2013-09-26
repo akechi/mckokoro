c4e73b93b8f131f3fbb4cbcb8ca3fc8a6b20ad8d
ujihisa <ujihisa@gmail.com>
2013-09-26 00:38:42 -0700

    w

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1437,7 +1437,7 @@ module EventHandler
         piston_block.type = Material::IRON_BLOCK
         piston_block.data = 0
         next_block = add_loc(piston_loc, x, 0, z).block
-        if next_block.solid? && next_block.type != Material::CHEST
+        if next_block.type.solid? && next_block.type != Material::CHEST
           next_block.type = ptype
           next_block.data = pdata
         end
