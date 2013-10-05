2adbd8193a34eb1247433fa4f7c6d658cfc475da
ujihisa <ujihisa@gmail.com>
2013-10-05 11:28:56 -0700

    tehepero

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -71,7 +71,7 @@ module Util
   end
 
   def post_lingr_to(room, text)
-    return if Bukkit.server.port == 25565 # TODO hard-coded mckokoro-dev
+    return if Bukkit.server.port == 25566 # TODO hard-coded mckokoro-dev
 
     Thread.start do
       # Send chat for lingr room
