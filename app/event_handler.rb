146cb2e93add4925f124e676715c565b1f98d4e8
ujihisa <ujihisa@gmail.com>
2013-10-06 22:24:10 -0700

    arrow

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -1523,7 +1523,7 @@ module EventHandler
       evt.cancelled = true
       player.send_message "sending items to #{phone_num}"
       items = player.get_nearby_entities(3, 3, 3).select {|e|
-        Item === e || Animals === e || Villager === e
+        Item === e || Animals === e || Villager === e || Arrow === e
       }
       unless items.empty?
         play_effect(loc, Effect::ENDER_SIGNAL, nil)
