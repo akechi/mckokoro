7d2a4c225437d72438288554bc3c30f05de14548
ujihisa <ujihisa@gmail.com>
2013-10-31 23:50:04 -0700

    downgrade

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject mckokoro "1.0.0-SNAPSHOT"
   :description "a minecraft server in JRuby w/o purugin"
   :dependencies [[org.bukkit/bukkit "1.6.4-R0.1-SNAPSHOT"]
-                 [org.jruby/jruby-complete "1.7.6"]]
+                 [org.jruby/jruby-complete "1.7.5"]]
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :repositories {"org.bukkit"
