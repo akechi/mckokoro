74d6ffe7a860cddca09204997732964914847b5d
ujihisa <ujihisa@gmail.com>
2013-11-03 00:58:09 +0900

    jump tweaks

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -2613,10 +2613,10 @@ module EventHandler
     if evt.sneaking?
       # counting up
       @crouching_counter[name] += 1
-      later sec(2.5) do
+      later sec(2.0) do
         @crouching_counter[name] -= 1
       end
-      if @crouching_counter[name] == 5
+      if @crouching_counter[name] == 4
         play_sound(add_loc(player.location, 0, 5, 0), Sound::BAT_TAKEOFF, 1.0, 0.0)
         # evt.player.send_message "superjump!"
         player.fall_distance = 0.0
