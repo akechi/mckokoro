173a055ea02c159feded5393848232ad12a15115
ujihisa <ujihisa@gmail.com>
2013-11-23 12:55:18 -0800

    Revert "test"
    This reverts commit 615ae89bd120d17194bef163fb557d8af0414022.

--- app/event_handler.rb
+++ app/event_handler.rb
@@ -338,18 +338,18 @@ module EventHandler
       post_lingr_to('computer_science', "#{evt.player.name}: #{message}")
     else
       evt.message = evt.message.
-        gsub(/[bv]enr[iy]/, '便利')#.
-        #gsub(/[fh]u[bv]en/, '不便').
-        #gsub(/wa-i/, 'わーい[^。^]').
-        #gsub(/dropper|ドロッパ/, '泥(・ω・)ﾉ■ ｯﾊﾟ').
-        #gsub(/hopper|ホッパ/, '穂(・ω・)ﾉ■ ｯﾊﾟ').
-        #gsub(/\bkiken/, '危険').
-        #gsub(/\banzen/, '安全').
-        #gsub(/wkwk/, '((o(´∀｀)o))ﾜｸﾜｸ').
-        #gsub(/unko/, 'unko大量生産!ブリブリo(-"-;)o⌒ξ~ξ~ξ~ξ~ξ~ξ~ξ~ξ~').
-        #gsub(/dks/, '溺((o(´o｀)o))死').
-        #gsub(/tkm/, '匠').
-        #sub(/^!\?$/, '!? な、なんだってーΩ ΩΩ')
+        gsub(/[bv]enr[iy]/, '便利').
+        gsub(/[fh]u[bv]en/, '不便').
+        gsub(/wa-i/, 'わーい[^。^]').
+        gsub(/dropper|ドロッパ/, '泥(・ω・)ﾉ■ ｯﾊﾟ').
+        gsub(/hopper|ホッパ/, '穂(・ω・)ﾉ■ ｯﾊﾟ').
+        gsub(/\bkiken/, '危険').
+        gsub(/\banzen/, '安全').
+        gsub(/wkwk/, '((o(´∀｀)o))ﾜｸﾜｸ').
+        gsub(/unko/, 'unko大量生産!ブリブリo(-"-;)o⌒ξ~ξ~ξ~ξ~ξ~ξ~ξ~ξ~').
+        gsub(/dks/, '溺((o(´o｀)o))死').
+        gsub(/tkm/, '匠').
+        sub(/^!\?$/, '!? な、なんだってーΩ ΩΩ')
       post_lingr("#{evt.player.name}: #{evt.message}")
       if evt.message == 'optifine'
         Bukkit.dispatch_command(Bukkit.get_console_sender, "toggledownfall")
